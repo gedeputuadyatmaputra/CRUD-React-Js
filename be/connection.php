b839368d143f2fa2b65cd5abab11672ef64c34b6
gedeputuadyatmaputra <100749323+gedeputuadyatmaputra@users.noreply.github.com>
2024-01-10 20:06:32 +0700

    Add files via upload

---
+++ be/connection.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $password ="";
+    $database = "pweb_gedeputuadyatmap";
+
+    $koneksi = mysqli_connect($host, $username, $password, $database);
+?>
